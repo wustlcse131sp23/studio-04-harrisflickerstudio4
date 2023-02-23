5222c467e72dbd57988b504f0c138f97b7b9e66e
betsyharris <betsyharris@localhost>
2023-02-23 12:23:34 -0600

    yayyyyy

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,14 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.clear();
+		StdDraw.setPenColor(255, 40, 170);
+		StdDraw.filledRectangle(.3, .3, .3, .3);
+		StdDraw.rectangle(3, 13, 3, 3);
+		StdDraw.rectangle(16, 3, 6, 3);
+		StdDraw.filledRectangle(.16, .13, .6, .3);
+		StdDraw.setPenColor(170, 40, 250);
+		StdDraw.rectangle(11, 8, 11, 1);
+		StdDraw.rectangle(8, 8, 1, 8);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,42 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		Color made = new Color (redComponent, greenComponent, blueComponent);
+		boolean isFilled = in.nextBoolean();
+		if (shapeType.equals("rectangle")) {
+			double parameterOne = in.nextDouble();
+			double parameterTwo = in.nextDouble();
+			double parameterThree = in.nextDouble();
+			double parameterFour = in.nextDouble();
+			if (isFilled) {
+				StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+			else {
+				StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+		}}
+		if (shapeType.equals("triangle")) {
+			double [] x = new double[3];
+			x [0] = in.nextDouble();
+			x [1] = in.nextDouble();
+			x [2] = in.nextDouble();
+			double [] y = new double[3];
+			y [0] = in.nextDouble();
+			y [1] = in.nextDouble();
+			y [2] = in.nextDouble();
+			StdDraw.setPenColor(made);
+			if (isFilled) {
+				StdDraw.filledPolygon(x, y);				 
+			}
+			else {
+				StdDraw.polygon(x,y);
+			
+			
 		
-	}
-}
+		
+		}
+	}}}
+
